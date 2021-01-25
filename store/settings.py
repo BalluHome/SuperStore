292257ada99ca3545b366edac31fed96f63ab984
Ahmed075 <akhmed096@mail.ru>
2021-01-25 21:46:58 +0600

    cart update

--- store/settings.py
+++ store/settings.py
@@ -150,7 +150,6 @@ EMAIL_HOST_USER = config('ACCOUNT')
 EMAIL_HOST_PASSWORD = config('PASSWORD')
 
 REST_FRAMEWORK = {
-
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 4,
     'DEFAULT_FILTER_BACKENDS': (
